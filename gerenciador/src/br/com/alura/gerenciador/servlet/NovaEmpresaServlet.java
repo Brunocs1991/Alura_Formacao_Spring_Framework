240097a87d8ebbe81b07cf46fa6a30d5936f2afe
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-18 19:45:26 -0300

    passando parametros requisição

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public NovaEmpresaServlet() {
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso !!</body></html>");		
+	}
+
+}
