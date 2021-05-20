abb2558a348476bfa1ee2dc0ec07f7dbbca7446b
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-20 10:20:38 -0300

    Listando empresas com servlets

--- gerenciador/src/br/com/alura/gerenciador/persistence/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/persistence/Banco.java
@@ -8,6 +8,16 @@ import br.com.alura.gerenciador.model.Empresa;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Alura");
+		lista.add(empresa1);
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa2);
+		
+	}
 
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -0,0 +1,39 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import br.com.alura.gerenciador.model.Empresa;
+import br.com.alura.gerenciador.persistence.Banco;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/listaEmpresas")
+public class ListaEmpresasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Banco banco = new Banco();
+		List<Empresa> lista = banco.getEmpresa();
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>"); 
+		out.println("<body>");
+		out.println("<ul>");
+			for(Empresa empresa : lista) {
+				out.println("<li>" + empresa.getNome() + "</li>");
+			}
+		out.println("</ul>");
+		out.println("</body>"); 
+		out.println("</html>"); 
+		
+		System.out.println("Servlet chamado");
+	}
+
+
+}
