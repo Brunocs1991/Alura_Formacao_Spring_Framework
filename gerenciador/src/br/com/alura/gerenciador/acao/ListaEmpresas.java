65280991611a5da904b91b8c555e7818724beafd
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-24 19:52:55 -0300

    Autorizando o acesso

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,26 +6,15 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas implements Acao{
 	public String  executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession sessao = request.getSession();
-		
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresa();
-
-		request.setAttribute("empresas", lista);
-		
+		request.setAttribute("empresas", lista);		
 		return "forward:listaEmpresas.jsp";
-
-
 	}
 }
