480c9f7d5758859c9366812733723f1bcfd64ad3
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-26 12:08:37 -0300

    Serviço com XML

--- gerenciador/src/br/com/alura/gerenciador/servlet/EmpresasServiceJson.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/EmpresasServiceJson.java
@@ -15,8 +15,8 @@ import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 
-@WebServlet("/empresas")
-public class EmpresasService extends HttpServlet {
+@WebServlet("/empresasJson")
+public class EmpresasServiceJson extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
