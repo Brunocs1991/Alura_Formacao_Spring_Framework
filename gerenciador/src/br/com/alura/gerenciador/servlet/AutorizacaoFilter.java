bea3b5a7688fdf0a7379b3c7cfd7fd1641260f2e
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-26 13:20:27 -0300

    Servlet container jetty

--- gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,7 +14,13 @@ import javax.servlet.http.HttpSession;
 
 //@WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
 
+	@Override
+	public void destroy() {}
+	
+	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
 		System.out.println("AutorizacaoFilter");
--- gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/MonitoramentoFilter.java
@@ -4,12 +4,19 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 //@WebFilter(urlPatterns = "/entrada")
 public class MonitoramentoFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void destroy() {}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
