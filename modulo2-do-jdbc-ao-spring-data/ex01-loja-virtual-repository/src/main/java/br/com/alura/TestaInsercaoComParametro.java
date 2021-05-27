3d05007b6432b12ab9fd88cd5f73ae87079de987
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-26 21:37:54 -0300

    Usando o try-with-resources

--- modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/TestaInsercaoComParametro.java
+++ modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/TestaInsercaoComParametro.java
@@ -10,48 +10,41 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
-		connection.setAutoCommit(false);
-		
-		try {
-			PreparedStatement preparedStatement = connection.prepareStatement(
-					"insert into produto (nome, descricao) values (?, ?)",
-					Statement.RETURN_GENERATED_KEYS
-					);
+		try (Connection connection = connectionFactory.recuperarConexao()) {
+			connection.setAutoCommit(false);
 
-			adicionarVariavel("SmartTv", "45 polegadas", preparedStatement);
-			adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
-			
-			connection.commit();
-			
-			preparedStatement.close();
-			connection.close();
-		} catch (Exception e) {
-				
-			e.printStackTrace();
-			connection.rollback();
-			System.out.println("ROLLBACK EXECUTADO");					
+			try (PreparedStatement preparedStatement = connection.prepareStatement(
+					"insert into produto (nome, descricao) values (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+				adicionarVariavel("SmartTv", "45 polegadas", preparedStatement);
+				adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+				connection.commit();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement)
 			throws SQLException {
-		
+
 		preparedStatement.setString(1, nome);
 		preparedStatement.setString(2, descricao);
+
 		/*
-		if(nome.equals("Radio")) {
-			throw new RuntimeException("Não foi possivel adicionar o produto");
-		}
-		*/
+		 * if(nome.equals("Radio")) { throw new
+		 * RuntimeException("Não foi possivel adicionar o produto"); }
+		 */
 
 		preparedStatement.execute();
 
-		ResultSet resultSet = preparedStatement.getGeneratedKeys();
-		while (resultSet.next()) {
-			Integer id = resultSet.getInt(1);
-			System.out.println("O id gerado foi: " + id);
+		try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+			while (resultSet.next()) {
+				Integer id = resultSet.getInt(1);
+				System.out.println("O id gerado foi: " + id);
+			}
 		}
-		resultSet.close();
 	}
 }
