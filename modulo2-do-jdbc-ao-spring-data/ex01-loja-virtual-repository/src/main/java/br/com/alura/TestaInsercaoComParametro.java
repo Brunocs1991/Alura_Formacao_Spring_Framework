24d0e217db6255da0610df2137e56e884ef09c45
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-26 21:29:15 -0300

    Lidando com commit e rollback

--- modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/TestaInsercaoComParametro.java
+++ modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/TestaInsercaoComParametro.java
@@ -13,23 +13,37 @@ public class TestaInsercaoComParametro {
 		Connection connection = connectionFactory.recuperarConexao();
 		connection.setAutoCommit(false);
 		
-		PreparedStatement preparedStatement = connection.prepareStatement(
-				"insert into produto (nome, descricao) values (?, ?)",
-				Statement.RETURN_GENERATED_KEYS
-				);
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(
+					"insert into produto (nome, descricao) values (?, ?)",
+					Statement.RETURN_GENERATED_KEYS
+					);
 
-		adicionarVariavel("SmartTv", "45 polegadas", preparedStatement);
-		adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+			adicionarVariavel("SmartTv", "45 polegadas", preparedStatement);
+			adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+			
+			connection.commit();
+			
+			preparedStatement.close();
+			connection.close();
+		} catch (Exception e) {
+				
+			e.printStackTrace();
+			connection.rollback();
+			System.out.println("ROLLBACK EXECUTADO");					
+		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement)
 			throws SQLException {
+		
 		preparedStatement.setString(1, nome);
 		preparedStatement.setString(2, descricao);
-		
+		/*
 		if(nome.equals("Radio")) {
 			throw new RuntimeException("Não foi possivel adicionar o produto");
 		}
+		*/
 
 		preparedStatement.execute();
 
