e6d0d51c7f54e6a002ec0ce0075925e8d223a56d
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-26 23:18:56 -0300

    corrigindo separando em pacotes e funcionalidades

--- modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/jdbc/TestaInsercao.java
+++ modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/jdbc/TestaInsercao.java
@@ -1,4 +1,4 @@
-package br.com.alura;
+package br.com.alura.jdbc;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
---
+++ modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository/src/main/java/br/com/alura/modelo/produto.java
@@ -0,0 +1,5 @@
+package br.com.alura.modelo;
+
+public class produto {
+
+}
