402b1e88bc813dd57927e0e099c75d1f18539684
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-28 17:13:09 -0300

    Estados no delete da entidade

--- modulo2-do-jdbc-ao-spring-data/ex02-jpa-hibernate-persistencia/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ modulo2-do-jdbc-ao-spring-data/ex02-jpa-hibernate-persistencia/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -19,5 +19,10 @@ public class CategoriaDao {
 	public void atualizar(Categoria categoria) {
 		this.em.merge(categoria);
 	}
+	
+	public void remover(Categoria categoria) {
+		categoria = em.merge(categoria);
+		this.em.remove(categoria);
+	}
 
 }
