3f35eaf012ecd197fa2ad18c928055aacb5a807a
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-28 10:28:09 -0300

    Revisando o projeto

--- modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository-view/src/main/java/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ modulo2-do-jdbc-ao-spring-data/ex01-loja-virtual-repository-view/src/main/java/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -42,28 +42,32 @@ public class CategoriaDAO {
 	}
 
 	public List<Categoria> listarComProduto() throws SQLException {
-		Categoria ultima = null;
-		List<Categoria> categorias = new ArrayList<>();
+		try {
+			Categoria ultima = null;
+			List<Categoria> categorias = new ArrayList<>();
 
-		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO " + "FROM CATEGORIA C "
-				+ "INNER JOIN PRODUTO P ON C.ID = P.CATEGORIA_ID";
+			String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO " + "FROM CATEGORIA C "
+					+ "INNER JOIN PRODUTO P ON C.ID = P.CATEGORIA_ID";
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.execute();
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.execute();
 
-			try (ResultSet rst = pstm.getResultSet()) {
-				while (rst.next()) {
-					if (ultima == null || !ultima.getNome().equals(rst.getString(2))) {
-						Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+				try (ResultSet rst = pstm.getResultSet()) {
+					while (rst.next()) {
+						if (ultima == null || !ultima.getNome().equals(rst.getString(2))) {
+							Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
 
-						categorias.add(categoria);
-						ultima = categoria;
+							categorias.add(categoria);
+							ultima = categoria;
+						}
+						Produto produto = new Produto(rst.getInt(3), rst.getString(4), rst.getString(5));
+						ultima.adicionar(produto);
 					}
-					Produto produto = new Produto(rst.getInt(3), rst.getString(4), rst.getString(5));
-					ultima.adicionar(produto);
 				}
+				return categorias;
 			}
-			return categorias;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
