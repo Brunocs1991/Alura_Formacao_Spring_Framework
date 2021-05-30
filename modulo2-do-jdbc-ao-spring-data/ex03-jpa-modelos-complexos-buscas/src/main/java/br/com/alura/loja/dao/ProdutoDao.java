6b53fdbcc6cf04c25f18a1e8ec2ee022eeaa0be6
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 11:55:16 -0300

    Consultas com Criteria API

--- modulo2-do-jdbc-ao-spring-data/ex03-jpa-modelos-complexos-buscas/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ modulo2-do-jdbc-ao-spring-data/ex03-jpa-modelos-complexos-buscas/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.loja.modelo.Produto;
 
@@ -63,13 +67,13 @@ public class ProdutoDao {
 		String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
 		
 		if (nome != null && !nome.trim().isEmpty()) {
-			jpql = " AND p.nome = : nome ";
+			jpql += " AND p.nome = : nome ";
 		}
 		if (preco != null) {
-			jpql = " AND p.preco = : preco ";
+			jpql += " AND p.preco = : preco ";
 		}
 		if (dataCadastro != null) {
-			jpql = " AND p.dataCadastro = : dataCadastro ";
+			jpql += " AND p.dataCadastro = : dataCadastro ";
 		}
 		
 		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
@@ -87,4 +91,26 @@ public class ProdutoDao {
 		return query.getResultList();
 	}
 
+	public List<Produto> buscarPorParametrosComCritaria(String nome, BigDecimal preco, LocalDate dataCadastro){
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);		
+		Root<Produto> from = query.from(Produto.class);
+		
+		Predicate filtros = builder.and();
+		
+		if (nome != null && !nome.trim().isEmpty()) {
+			filtros = builder.and(filtros, builder.equal(from.get("nome"), nome));
+		}
+		if (preco != null) {
+			filtros = builder.and(filtros, builder.equal(from.get("preco"), preco));
+		}
+		if (dataCadastro != null) {
+			filtros = builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
+		}
+		
+		query.where(filtros);
+		
+		return em.createQuery(query).getResultList();
+	}
 }
