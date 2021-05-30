b5d6528cd8a908c56bfb8e5990b61182b3d24fa6
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 16:08:39 -0300

    Inserindo valores

--- modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class Ex04SpringDataJpaApplication {
+public class Ex04SpringDataJpaApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+
+	public Ex04SpringDataJpaApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex04SpringDataJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);
 	}
 
 }
