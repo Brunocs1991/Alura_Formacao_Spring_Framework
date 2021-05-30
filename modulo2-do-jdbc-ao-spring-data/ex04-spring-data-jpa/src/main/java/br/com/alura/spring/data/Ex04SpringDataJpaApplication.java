ccee5ffb7a18601fcde7635118f7e73245d71a38
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 16:30:08 -0300

    Salvando o registro

--- modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
@@ -1,19 +1,21 @@
 package br.com.alura.spring.data;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 
 @SpringBootApplication
 public class Ex04SpringDataJpaApplication implements CommandLineRunner {
 
-	private final CargoRepository repository;
+	private final CrudCargoService cargoService;
+	private Boolean system = true; 
 
-	public Ex04SpringDataJpaApplication(CargoRepository repository) {
-		this.repository = repository;
+	public Ex04SpringDataJpaApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 
 	public static void main(String[] args) {
@@ -22,9 +24,22 @@ public class Ex04SpringDataJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		repository.save(cargo);
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(system) {
+			System.out.println("Qual ação voce quer executar? ");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+			
+			int action = scanner.nextInt();
+			if(action == 1) {
+				cargoService.inicial(scanner);
+			}else {
+				system = false;
+			}
+		}
+		
 	}
 
 }
