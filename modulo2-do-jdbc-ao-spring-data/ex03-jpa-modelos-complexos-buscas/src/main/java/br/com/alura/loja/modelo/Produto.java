1fa0560c82dd6f72d0b0a5991df959e53c6f23e8
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 12:34:49 -0300

    Mapeamento de chaves compostas

--- modulo2-do-jdbc-ao-spring-data/ex03-jpa-modelos-complexos-buscas/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ modulo2-do-jdbc-ao-spring-data/ex03-jpa-modelos-complexos-buscas/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -11,12 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
-@NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+//@NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Produto {
 
