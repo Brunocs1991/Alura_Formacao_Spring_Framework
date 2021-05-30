c6e70de56505f7402c4dcc8da3c2e0c030da55d3
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 14:56:56 -0300

    Criando a aplicação

---
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex04SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex04SpringDataJpaApplication.class, args);
+	}
+
+}
