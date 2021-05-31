fe995bb6817502b476d3eb47711cdbd5b31bd2bc
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 22:03:01 -0300

    Executando a pesquisa dinâmica

--- modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/Ex04SpringDataJpaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatorioFuncionarioDinamico;
 import br.com.alura.spring.data.service.RelatoriosService;
 
 @SpringBootApplication
@@ -20,13 +21,16 @@ public class Ex04SpringDataJpaApplication implements CommandLineRunner {
 	private final CrudFuncionarioService funcionarioService;
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
 	private final RelatoriosService relatoriosService;
+	private final RelatorioFuncionarioDinamico relatorioFuncionarioDinamico;
 
 	public Ex04SpringDataJpaApplication(CrudCargoService cargoService, CrudFuncionarioService funcionarioService,
-			CrudUnidadeTrabalhoService unidadeTrabalhoService, RelatoriosService relatoriosService) {
+			CrudUnidadeTrabalhoService unidadeTrabalhoService, RelatoriosService relatoriosService,
+			RelatorioFuncionarioDinamico relatorioFuncionarioDinamico) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
 		this.relatoriosService = relatoriosService;
+		this.relatorioFuncionarioDinamico = relatorioFuncionarioDinamico;
 	}
 
 	public static void main(String[] args) {
@@ -45,8 +49,8 @@ public class Ex04SpringDataJpaApplication implements CommandLineRunner {
 			System.out.println("2 - Unidade");
 			System.out.println("3 - Funcionario");
 			System.out.println("4 - Relatorios");
+			System.out.println("5 - Relatorio Dinamico");
 			
-
 			Integer function = scanner.nextInt();
 
 			switch (function) {
@@ -61,6 +65,9 @@ public class Ex04SpringDataJpaApplication implements CommandLineRunner {
 				break;
 			case 4:
 				relatoriosService.inicial(scanner);
+				break;
+			case 5:
+				relatorioFuncionarioDinamico.inicial(scanner);
 				break;
 			default:
 				System.out.println("Finalizando");
