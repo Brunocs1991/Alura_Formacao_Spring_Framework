d67e9e8fbb2c66ee86fce90942d6ef064d3b2620
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-31 20:10:46 -0300

    Percorrendo a lista na view

--- modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,13 +1,28 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		
+		pedido.setNomeProduto("Smartphone Xiaomi Redmi Note 8 ");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/51Fv-PIiDQL._AC_SL1000_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com .br/Smartphone-Xiaomi-Redmi-Note-4RAM/dp/B07Y9XWK4M/ref=asc_df_B07Y9XWK4M/?tag=googleshopp00-20&linkCode=df0&hvadid=379792730378&hvpos=&hvnetw=g&hvrand=6005966472146014755&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1031969&hvtargid=pla-824195334382&psc=1");
+		pedido.setDescricao("Uma descricao qualquer para este pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
 }
