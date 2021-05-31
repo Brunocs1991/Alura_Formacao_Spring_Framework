5d4f52a9e8d7a6efe90aec32f38accd395ac03ad
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-30 21:41:16 -0300

    Estrutura da Specification

--- modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -3,13 +3,14 @@ package br.com.alura.spring.data.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
-public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer>,JpaSpecificationExecutor<Funcionario> {
 	// derived query
 	List<Funcionario> findByNome(String nome);
 
@@ -21,8 +22,9 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
 	
-	// projecao
-	
+	// projecao	
 	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
 	List<FuncionarioProjecao> findFuncionarioSalario();
+	
+	
 }
---
+++ modulo2-do-jdbc-ao-spring-data/ex04-spring-data-jpa/src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -0,0 +1,28 @@
+package br.com.alura.spring.data.service;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.repository.FuncionarioRepository;
+import br.com.alura.spring.data.specification.SpecificationFuncionario;
+
+public class RelatorioFuncionarioDinamico {
+
+	private final FuncionarioRepository funcionarioRepository;
+
+	public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
+		this.funcionarioRepository = funcionarioRepository;
+	}
+	
+	public void inicial(Scanner scanner) {
+		System.out.println("Digite um nome");
+		String nome = scanner.next();
+		
+		List<Funcionario> funcionarios = funcionarioRepository
+				.findAll(Specification.where(SpecificationFuncionario.nome(nome))); 
+	}
+
+}
