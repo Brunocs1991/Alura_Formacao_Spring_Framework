392cc5a505aa72e302211171b627c5735731284d
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-05-31 22:35:13 -0300

    Usando Spring Data JPA

--- modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -14,7 +14,8 @@ public class Pedido {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
+	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;
--- modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex01-spring-mvc-web/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,23 +1,10 @@
 package br.com.alura.mvc.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
-
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Pedido> recuperaTodosOsPedidos() {
-		Query query = entityManager.createQuery("SELECT p from Pedido p");
-		return query.getResultList();
-	}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
