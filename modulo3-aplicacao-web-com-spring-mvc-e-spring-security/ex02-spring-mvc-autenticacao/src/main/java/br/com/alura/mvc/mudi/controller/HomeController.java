fac3f61a5c5450bb2ff32e86c046a602d7c12366
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-03 17:42:04 -0300

    Controlando os usuários

--- modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex02-spring-mvc-autenticacao/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ modulo3-aplicacao-web-com-spring-mvc-e-spring-security/ex02-spring-mvc-autenticacao/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -25,17 +25,8 @@ public class HomeController {
 	@GetMapping
 	public String home(Model model, Principal principal) {
 
-		
-		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
 		model.addAttribute("pedidos", pedidos);
-		return "home";
-	}
-
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status") String status, Model model) {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-		model.addAttribute("pedidos", pedidos);
-		model.addAttribute("status", status);
 		return "home";
 	}
 
