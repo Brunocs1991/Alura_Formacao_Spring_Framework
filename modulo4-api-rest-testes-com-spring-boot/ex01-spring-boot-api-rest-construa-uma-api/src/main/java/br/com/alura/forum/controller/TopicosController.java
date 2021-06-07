835a63a46909e1e2abeb71ce89c76289c5c43612
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-07 14:45:16 -0300

    Usando DTO

--- modulo4-api-rest-testes-com-spring-boot/ex01-spring-boot-api-rest-construa-uma-api/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ modulo4-api-rest-testes-com-spring-boot/ex01-spring-boot-api-rest-construa-uma-api/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,21 +3,20 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 
-		Topico topico = new Topico("Duvida", "Duvida Spring", new Curso("Spring", "programacao"));
-		return Arrays.asList(topico, topico, topico);
+		Topico topico = new Topico("Duvida", "Dúvida Spring", new Curso("Spring", "programacao"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
