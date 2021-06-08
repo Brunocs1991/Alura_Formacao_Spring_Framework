c57d4bebc9301c1f10bc2ed839e02f26c8e2b35d
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-07 21:22:35 -0300

    Removendo tópicos

--- modulo4-api-rest-testes-com-spring-boot/ex01-spring-boot-api-rest-construa-uma-api/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ modulo4-api-rest-testes-com-spring-boot/ex01-spring-boot-api-rest-construa-uma-api/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class TopicosController {
 	}
 
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
 		Topico topico = form.converter(cursoRepository);
@@ -61,12 +63,19 @@ public class TopicosController {
 		Topico topico = topicoRepository.getOne(id);
 		return new DetalhesTopicoDto(topico);
 	}
-	
+
 	@PutMapping(value = "/{id}")
 	@Transactional
-	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
-		
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+
 		Topico topico = form.atualizar(id, topicoRepository);
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
