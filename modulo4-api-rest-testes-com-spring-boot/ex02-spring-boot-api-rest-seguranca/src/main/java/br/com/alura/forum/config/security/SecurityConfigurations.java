d52cd30ffe9bda7f25e8d5bcd493721121ebc30b
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-09 15:33:01 -0300

    Documentação da API com SpringFox Swagger

--- modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -50,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 				.antMatchers(HttpMethod.POST, "/auth").permitAll()
 				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				
 				.anyRequest().authenticated()
 				.and().csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -59,6 +60,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// configurar parte de recursos staticos
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "configuration/**", "/swagger-resources/**");
 	}
 }
