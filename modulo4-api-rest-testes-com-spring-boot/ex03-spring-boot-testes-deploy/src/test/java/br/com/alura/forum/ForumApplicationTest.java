7869d046316a2fccb7da56157d1a2c33a8625a6b
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-09 18:23:58 -0300

    testando um repository

--- modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/ForumApplicationTest.java
+++ modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/ForumApplicationTest.java
@@ -1,18 +1,17 @@
-package br.com.alura;
+package br.com.alura.forum;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ForumApplication {
+public class ForumApplicationTest {
 
 	@Test
 	public void contextLoads() {
 		Assert.assertTrue(true);
 	}
-
 }
