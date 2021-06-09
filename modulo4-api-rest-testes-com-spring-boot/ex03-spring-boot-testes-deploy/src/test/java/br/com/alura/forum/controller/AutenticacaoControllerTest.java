8c91dec2bc8c3e28d155c3ebd7d6680fa288e7ce
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-09 20:10:32 -0300

    gerando o jar da aplicação

--- modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
+++ modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -3,18 +3,18 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
--- modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ modulo4-api-rest-testes-com-spring-boot/ex03-spring-boot-testes-deploy/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,18 +1,21 @@
 package br.com.alura.forum.repository;
 
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.alura.forum.modelo.Curso;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
@@ -33,15 +36,15 @@ public class CursoRepositoryTest {
 		em.persist(html5);
 		
 		Curso curso = repository.findByNome(nomeCurso);
-		Assert.assertNotNull(curso);
-		Assert.assertEquals(nomeCurso, curso.getNome());
+		assertNotNull(curso);
+		assertEquals(nomeCurso, curso.getNome());
 	}
 	
 	@Test
 	public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
 		String nomeCurso = "JPA";
 		Curso curso = repository.findByNome(nomeCurso);
-		Assert.assertNull(curso);
+		assertNull(curso);
 	}
 
 }
