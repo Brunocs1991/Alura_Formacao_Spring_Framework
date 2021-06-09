1426da12fa2d81aa48984cd18451602256832536
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-09 10:17:04 -0300

    Autenticando o usuário

--- modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/resources/data.sql
+++ modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$60aFfGM2zoylaaUXlXBscuhnxvkEgZb0fmeRem3ZFOO3OJGhfqxqG');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
