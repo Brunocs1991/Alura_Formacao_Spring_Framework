bf0bf1a387ce0d2103931b26b0844766697610c1
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-09 12:46:38 -0300

    Recuperando o token do header Authorization

--- modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ modulo4-api-rest-testes-com-spring-boot/ex02-spring-boot-api-rest-seguranca/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -36,10 +37,14 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	// configurar parte de autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.anyRequest().authenticated()
+				.and().csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	// configurar parte de recursos staticos
